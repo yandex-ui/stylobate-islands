2290a334d62c82993662a2daa06dbb40fec03cd5
Roman Komarov <kizu@yandex-team.ru>
2014-10-02 18:23:08 +0400

    Removed all the extra stuff for testing, replaced with basic-basic postcss, changed all the tests

--- test-runner.js
+++ test-runner.js
@@ -4,17 +4,14 @@
 
 var stylus = require('stylus');
 var fs = require('fs');
-var csso = require('csso');
+var postcss = require('postcss');
 var autoprefixer = require('autoprefixer');
 var glob = require('glob');
-var Comb = require('csscomb');
 var whatToTest = process.env.npm_package_config_whatToTest || '**';
-var comb = new Comb();
-comb.configure(require('./.csscomb.json'));
 
 // test cases
 
-glob.sync("./lib/**/" + whatToTest + "/tests/*.styl").forEach(function(test){
+glob.sync("./lib/*/" + whatToTest + "/tests/*.styl").forEach(function(test){
   var name = test.replace(/\.?[\/]/g, ' ').replace(' tests',':').replace('.styl','');
   if(name.indexOf(' old_') != -1) {
     return;
@@ -22,31 +19,23 @@ glob.sync("./lib/**/" + whatToTest + "/tests/*.styl").forEach(function(test){
 
   it(name, function(){
     var css = fs.readFileSync(test.replace('.styl', '.css'), 'utf8').replace(/\r/g, '').trim();
-    var style = stylus('@import "lib/standalone.styl"; @import "' + test + '"');
+    var style = stylus('@require "lib/standalone.styl"; @require "' + test + '"');
 
     style.render(function(err, actual){
       if (err) throw err;
-      actual = autoprefixer("last 2 versions", "> 2%").process(actual).css;
-      actual = csso.justDoIt(actual);
-      actual = comb.processString(actual);
 
-      // Remove those hardfixes when there would be a way to do this in csscomb
-      actual = actual.replace(/([^\+>])([\+>])\./g,'$1 $2 .');
-      actual = actual.replace(/\)(,?)([^:\)\s,;])/g,')$1 $2');
-      actual = actual.replace(/, ?(\.|label|input|x\:)/g,',\n$1');
-      actual = actual.replace(/([^ ])\}/g,'$1}\n');
-      actual = actual.replace(/,(\S)/g,', $1');
-      actual = actual.replace(/base64, /g,'base64,');
-      actual = actual.replace(/background: 0 0/g,'background: transparent');
-      actual = actual.replace(/font-weight: 700/g,'font-weight: bold');
-      actual = actual.replace(/([^ ])!important/g,'$1 !important');
-      actual = actual.replace(/inset,([^ ])/g,'inset, $1');
-      actual = actual.replace(/\)rgba/g,') rgba');
+      actual = postcss()
+          .use( autoprefixer().postcss )
+          .process(actual);
 
-      // CSSO strips ie9 hack, should replace with smth else
-      actual = actual.replace(/color: #000 \\0\/;/g,'color: transparent;\n  color: #000 \\0/;');
+      actual.root.eachRule(function (rule) {
+          // Insert an extra newline before each non-first rule
+          if (rule.parent.rules[0] !== rule) {
+              rule.before = '\n\n';
+          }
+      });
 
-      actual.trim().should.equal(css);
+      actual.css.trim().should.equal(css);
     });
   })
 });
